d06980181cb823fd4e862656df3c22af417766a7
Romain2109 <contact@romain-voileaux.fr>
2024-06-02 15:28:19 +0200

    V1

--- email-gate.php
+++ email-gate.php
@@ -3,7 +3,7 @@
 Plugin Name: Email Gate
 Description: Plugin pour vérifier l'adresse email des visiteurs avant de donner accès au contenu du site.
 Version: 1.0
-Author: Romain Voileaux - RVLX
+Author: Votre Romain Voileaux - RVLX
 GitHub Plugin URI: https://github.com/Romain2109/email-gate-wp
 GitHub Branch: main
 */
@@ -16,4 +16,12 @@ if (!defined('ABSPATH')) {
 include_once plugin_dir_path(__FILE__) . 'includes/admin.php';
 include_once plugin_dir_path(__FILE__) . 'includes/form.php';
 include_once plugin_dir_path(__FILE__) . 'includes/verification.php';
+
+// Démarrer la session
+function eg_start_session() {
+    if (!session_id()) {
+        session_start();
+    }
+}
+add_action('init', 'eg_start_session');
 ?>
