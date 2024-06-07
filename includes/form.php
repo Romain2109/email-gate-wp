fb88cdb4ee7cce92cc9a7a70956c0bbecaef9725
Romain2109 <contact@romain-voileaux.fr>
2024-06-07 14:42:46 +0200

    Style fix error

--- includes/form.php
+++ includes/form.php
@@ -11,7 +11,8 @@ if (!function_exists('email_gate_form')) {
                 wp_redirect($redirect_url);
                 exit;
             } else {
-                echo '<div class="error"><p>Vous n\'avez pas le droit d\'accéder à ce site.</p></div>';
+                echo '<div id="error-flash" class="error"><span>Email invalide ! </span></div>';
+
             }
         }
 
@@ -37,22 +38,26 @@ if (!function_exists('email_gate_form')) {
         
         * {
             margin: 0;
-            padding: 0;
             box-sizing: border-box;
-       }
+           }
         body {
+            display: flex;
+            justify-content: center;
             font-family: "Roboto";
             font-size: 14px;
             background-size: 200% 100% !important;
             animation: move 10s ease infinite;
             transform: translate3d(0, 0, 0);
             background: linear-gradient(45deg, ' . $color1_gradient_form . ' 10%, ' . $color2_gradient_form . ' 90%);
-            height: 100vh;
        }
+        .container{
+            align-content: center;
+            width: 100%;
+        }
         .user {
             width: 90%;
             max-width: 340px;
-            margin: 10vh auto;
+            margin: 0 auto;
        }
         .user__header {
             text-align: center;
@@ -155,35 +160,39 @@ if (!function_exists('email_gate_form')) {
            }
        }
 
-       /* Media Queries for Responsiveness */
-       @media (max-width: 768px) {
-           .user {
-               width: 80%;
-           }
-       
-           .form__input,
-           .btn {
-               padding: 15px;
-           }
+       .error{
+        position: absolute;
+        background-color: white;
+        padding: 20px;
+        border-radius: 6px;
+        margin-top: 1%;
+        margin-left: 1%;
+        margin-right: 1%;
+        width: 98%;
        }
-       
+
+       .error > span{
+        font-weight: 500;
+       }
         </style>
         ';
+        echo '
+        <div class="container">
+            <div class="user">
+                <header class="user__header">
+                    <img src="' . esc_url(wp_get_attachment_url(get_option('logo_form'))) . '" alt="" />
+                    <h1 class="user__title">' . $title_form . '</h1>
+                </header>
+                <form method="post" class="form">
+                    <div class="form__group">
+                        <input type="email" name="email_gate_email" placeholder="Email" class="form__input" required />
+                        <input type="hidden" name="redirect_to" value="' . esc_url($_SERVER['REQUEST_URI']) . '">
+                        <input class="btn" type="submit" value="Valider">
+                    </div>
+                </form>
+            </div>
+        </div>';
 
-        echo '<div class="user">';
-        echo '<header class="user__header">';
-        echo '<img src="' . esc_url(wp_get_attachment_url(get_option('logo_form'))) . '" alt="" />';
-        echo '<h1 class="user__title">' . $title_form . '</h1>';
-        echo '</header>';
-    
-        echo '<form method="post" class="form">';
-        echo '<div class="form__group">';
-        echo '<input type="email" name="email_gate_email" placeholder="Email" class="form__input" required />';
-        echo '<input type="hidden" name="redirect_to" value="' . esc_url($_SERVER['REQUEST_URI']) . '">';
-        echo '<input class="btn" type="submit" value="Valider">';
-        echo '</div>';
-        echo '</form>';
-        echo '</div>';
     }
 }
 ?>
